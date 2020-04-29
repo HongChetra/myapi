7e8ded08814dd0914c9854cd2cac145ee3fa8e72
HongChetra <58508518+HongChetra@users.noreply.github.com>
2020-04-29 22:30:43 +0700

    Changed DB location

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ from security import authenticate, identity
 from resources.user import UserRegister 
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 app = Flask(__name__)
-
-db.init_app(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,12 +20,6 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity) 
 
 
-
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
@@ -36,6 +28,6 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
-	#from db import db 
-	#db.init_app(app)
-	app.run(port=4000, debug=True)
+	from db import db 
+	db.init_app(app)
+	app.run()
